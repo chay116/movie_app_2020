3529b554f4c4be2906e9a51f1aeb3ba8353cfafd
chay116 <chay116@naver.com>
2020-06-16 13:51:36 +0900

    Updates

---
+++ precache-manifest.89f0b22c2fb26d25122c84348415d466.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79eca5234db16a5438bcfcb34789d9ba",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "57b4ec443a8523354e53",
+    "url": "/movie_app_2020/static/css/main.2a819363.chunk.css"
+  },
+  {
+    "revision": "7a8a201905200cb51084",
+    "url": "/movie_app_2020/static/js/2.3b8dd4b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.3b8dd4b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57b4ec443a8523354e53",
+    "url": "/movie_app_2020/static/js/main.c37b2f17.chunk.js"
+  },
+  {
+    "revision": "d1cc10a2b60c92ec271c",
+    "url": "/movie_app_2020/static/js/runtime-main.5d7b76c9.js"
+  }
+]);
